22e3c8d8b03b230edb721e4927f5a9ab1bbec421
Abhinandan Mittal <abhinandan1812@gmail.com>
2020-12-07 22:11:14 +0530

    Adding Template files

---
+++ src/com/abnndn/template/First.java
@@ -0,0 +1,25 @@
+package com.abnndn.template;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class First {
+    public static void calculateAnswer() {
+        List<Integer> input = new ArrayList<>();
+        try {
+            File myObj = new File("/Users/abhmitta/Desktop/AdventOfCode2020/src/com/abnndn/<day>/input.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                input.add(Integer.valueOf(data));
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
